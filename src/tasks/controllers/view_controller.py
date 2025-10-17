3ba60f88bcc45d6487546a5b49ca67a6c629b891
Fillipe Goulart <fillipe.gsm@tutanota.com>
2025-10-17 18:24:51 -0300

    Add view for a single task with details

---
+++ src/tasks/controllers/view_controller.py
@@ -0,0 +1,13 @@
+from src.tasks.models.task import Task
+from src.tasks.presenters.no_task_presenter import NoTaskPresenter
+from src.tasks.presenters.view_presenter import ViewPresenter
+
+
+def view_controller(task_id: int) -> None:
+    """Shows a single task with full details"""
+    task = Task.get_or_none(Task.id == task_id)
+    if not task:
+        NoTaskPresenter().present(task_id=task_id)
+        return
+
+    ViewPresenter().present(task)
---
+++ tests/tasks/controllers/test_view_controller.py
@@ -0,0 +1,40 @@
+from unittest.mock import patch
+
+import pytest
+
+from src.tasks.controllers.view_controller import view_controller
+from src.tasks.models.task import Task
+from src.tasks.presenters.no_task_presenter import NoTaskPresenter
+from src.tasks.presenters.view_presenter import ViewPresenter
+
+
+@pytest.fixture
+def mocked_no_task_presenter():
+    with patch.object(NoTaskPresenter, "present") as m:
+        yield m
+
+
+@pytest.fixture
+def mocked_view_presenter():
+    with patch.object(ViewPresenter, "present") as m:
+        yield m
+
+
+def test_no_tasks_exist(
+    tmp_db, mocked_no_task_presenter, mocked_view_presenter
+):
+    """As no tasks exist, we should see the appropriate message"""
+    view_controller(task_id=1)
+
+    mocked_no_task_presenter.assert_called_once()
+    mocked_view_presenter.assert_not_called()
+
+
+def test_tasks_exist(tmp_db, mocked_no_task_presenter, mocked_view_presenter):
+    task = Task.create(title="task1")
+
+    view_controller(task_id=task.id)
+
+    # As there is a task, it calls the "view task presenter"
+    mocked_no_task_presenter.assert_not_called()
+    mocked_view_presenter.assert_called_once()
