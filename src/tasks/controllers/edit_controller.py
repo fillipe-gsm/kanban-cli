6a237699d367570c8d07fdaef95aa9e5dc66463a
Fillipe Goulart <fillipe.gsm@tutanota.com>
2025-10-19 09:15:20 -0300

    Create edit controller
    Existing tasks should be able to be edited now

---
+++ src/tasks/controllers/edit_controller.py
@@ -0,0 +1,36 @@
+from src.tasks.controllers.view_controller import view_controller
+from src.tasks.models.category import Category
+from src.tasks.models.task import Task
+from src.tasks.presenters.no_task_presenter import NoTaskPresenter
+from src.tasks.prompts.category_prompt import CategoryPrompt
+from src.tasks.prompts.details_prompt import DetailsPrompt
+from src.tasks.prompts.status_prompt import StatusPrompt
+from src.tasks.prompts.title_prompt import TitlePrompt
+
+
+def edit_controller(task_id: int) -> None:
+    """Run a sequence of prompts to edit an existing task"""
+    task: Task = Task.get_or_none(Task.id == task_id)
+    if not task:
+        NoTaskPresenter().present(task_id=task_id)
+        return
+
+    title = TitlePrompt(default_value=task.title).prompt()
+    status = StatusPrompt(default_value=task.status).prompt()
+    category_names = Category.category_names()
+    category_name = CategoryPrompt(
+        default_value=task.category_name, category_names=category_names
+    ).prompt()
+    details = DetailsPrompt(
+        default_value=task.details, is_editing=True
+    ).prompt()
+
+    task.edit_from_prompt(
+        title=title,
+        status=status,
+        category_name=category_name,
+        details=details,
+    )
+
+    # Print the updated task in full
+    view_controller(task_id=task.id)
---
+++ tests/tasks/controllers/test_edit_controller.py
@@ -0,0 +1,157 @@
+from unittest.mock import patch
+
+import pytest
+
+from src.tasks.controllers.edit_controller import edit_controller
+from src.tasks.models.category import Category
+from src.tasks.models.task import Task
+from src.tasks.presenters.no_task_presenter import NoTaskPresenter
+from tests.tasks.prompts.key_mappings import KEY_MAPPINGS
+
+
+@pytest.fixture
+def mocked_view_all_controller():
+    with patch("src.tasks.controllers.edit_controller.view_controller") as m:
+        yield m
+
+
+@pytest.fixture
+def mocked_no_task_presenter():
+    with patch.object(NoTaskPresenter, "present") as m:
+        yield m
+
+
+def test_no_tasks_exist(
+    tmp_db, mocked_no_task_presenter, mocked_view_all_controller
+):
+    """As no tasks exist, we should see the appropriate message"""
+    edit_controller(task_id=1)
+
+    mocked_no_task_presenter.assert_called_once()
+    mocked_view_all_controller.assert_not_called()
+
+
+def test_edit_defaults(
+    tmp_db,
+    mocked_no_task_presenter,
+    mocked_view_all_controller,
+    mocked_prompt_input,
+    mock_app_session,
+):
+    """Test we get an updated task with no changes"""
+    category = Category.create(name="category")
+    task = Task.create(
+        title="Buy milk",
+        status=1,
+        category=category,
+        details="Buy milk because...",
+    )
+    assert Category.select().count() == 1, "sanity check: one category exists"
+
+    mocked_prompt_input.send_text(KEY_MAPPINGS["<Enter>"])  # accept title
+    mocked_prompt_input.send_text(KEY_MAPPINGS["<Enter>"])  # accept status
+    mocked_prompt_input.send_text(KEY_MAPPINGS["<Enter>"])  # accept category
+    mocked_prompt_input.send_text(
+        KEY_MAPPINGS["<Enter>"]
+    )  # don't edit details
+
+    with mock_app_session:
+        edit_controller(task_id=task.id)
+
+    edited_task = Task.get_by_id(pk=task.id)
+
+    # Sanity check: no property got changed
+    assert edited_task.title == task.title
+    assert edited_task.status == task.status
+    assert edited_task.category == task.category
+    assert edited_task.details == task.details
+    assert Category.select().count() == 1, (
+        "sanity check: no new category was created"
+    )
+
+
+def test_edit_defaults__can_update_attribute(
+    tmp_db,
+    mocked_no_task_presenter,
+    mocked_view_all_controller,
+    mocked_prompt_input,
+    mock_app_session,
+):
+    category = Category.create(name="category")
+    task = Task.create(
+        title="Buy milk",
+        status=1,
+        category=category,
+        details="Buy milk becausi",  # with a typo at the last character
+    )
+    assert Category.select().count() == 1, "sanity check: one category exists"
+
+    mocked_prompt_input.send_text(KEY_MAPPINGS["<Enter>"])  # accept title
+    mocked_prompt_input.send_text(KEY_MAPPINGS["<Enter>"])  # accept status
+    mocked_prompt_input.send_text(KEY_MAPPINGS["<Enter>"])  # accept category
+
+    # For details: press "Down" to select "yes"
+    mocked_prompt_input.send_text(KEY_MAPPINGS["<Down>"])  # accept category
+    mocked_prompt_input.send_text(KEY_MAPPINGS["<Enter>"])
+
+    # Press backspace to fix the typo and add a couple more characters
+    mocked_prompt_input.send_text(KEY_MAPPINGS["<Backspace>"])
+    mocked_prompt_input.send_text("e...")
+    # Press Esc+Enter to confirm
+    mocked_prompt_input.send_text(KEY_MAPPINGS["<Esc>"])
+    mocked_prompt_input.send_text(KEY_MAPPINGS["<Enter>"])
+
+    with mock_app_session:
+        edit_controller(task_id=task.id)
+
+    edited_task = Task.get_by_id(pk=task.id)
+
+    # Sanity check: no property but the details got changed
+    assert edited_task.title == task.title
+    assert edited_task.status == task.status
+    assert edited_task.category == task.category
+    assert edited_task.details == "Buy milk because...", "details got updated"
+    assert Category.select().count() == 1, (
+        "sanity check: no new category was created"
+    )
+
+
+def test_edit_defaults__can_update_category(
+    tmp_db,
+    mocked_no_task_presenter,
+    mocked_view_all_controller,
+    mocked_prompt_input,
+    mock_app_session,
+):
+    category = Category.create(name="category 1")
+    task = Task.create(
+        title="Buy milk",
+        status=1,
+        category=category,
+    )
+    assert Category.select().count() == 1, "sanity check: one category exists"
+
+    mocked_prompt_input.send_text(KEY_MAPPINGS["<Enter>"])  # accept title
+    mocked_prompt_input.send_text(KEY_MAPPINGS["<Enter>"])  # accept status
+
+    # Edit category name
+    mocked_prompt_input.send_text(KEY_MAPPINGS["<Backspace>"])
+    mocked_prompt_input.send_text("2")  # edit to "category 2"
+    mocked_prompt_input.send_text(KEY_MAPPINGS["<Enter>"])
+
+    mocked_prompt_input.send_text(
+        KEY_MAPPINGS["<Enter>"]
+    )  # don't change details
+
+    with mock_app_session:
+        edit_controller(task_id=task.id)
+
+    edited_task = Task.get_by_id(pk=task.id)
+
+    assert Category.select().count() == 2, "one new category was created"
+    assert edited_task.category_name == "category 2"
+
+    # Sanity check: no property but the category got changed
+    assert edited_task.title == task.title
+    assert edited_task.status == task.status
+    assert edited_task.details == task.details
