4899e0eee742280542edfb0302fc7400af18105e
Fillipe Goulart <fillipe.gsm@tutanota.com>
2025-11-01 10:22:13 -0300

    Move message to variable for better reuse

--- src/tasks/presenters/no_task_presenter.py
+++ src/tasks/presenters/no_task_presenter.py
@@ -4,8 +4,10 @@ from rich.console import Console
 class NoTaskPresenter:
     """A helper presenter showing that no task matches a given id"""
 
+    MSG = "No task exists with id #{}."
+
     def __init__(self, console: Console | None = None) -> None:
         self._console = console or Console()
 
     def present(self, task_id: int) -> None:
-        self._console.print(f"No task exists with id #{task_id}.")
+        self._console.print(self.MSG.format(task_id))
