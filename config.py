4733e735e9a2d28bc490adff1710c896d1e54467
Fillipe Goulart <fillipe.gsm@tutanota.com>
2025-11-08 17:31:03 -0300

    Allow possibility to overwrite variables
    The user can overwrite everything by creating a `kb_settings.toml` file
    instead of just creating environment variables

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
 from dynaconf import Dynaconf
 
+# The user can overwrite existing variables with either:
+# - creating environment variables starting with `KB_` such as `KB_DB_NAME`; or
+# - create a `kb_settings.toml` and add the variables they want
 settings = Dynaconf(
     envvar_prefix="KB",
-    settings_files=["settings.toml", ".secrets.toml"],
+    settings_files=["settings.toml", ".secrets.toml", "kb_settings.toml"],
 )
 
 # `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
